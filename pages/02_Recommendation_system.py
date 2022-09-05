062b40df288864eb6253d7d73b0b481b4c85da08
DorisBaillard <baillard.doris@gmail.com>
2022-09-05 22:33:12 +0200

    new version  pages/02_Recommendation_system.py

--- pages/02_Recommendation_system.py
+++ pages/02_Recommendation_system.py
@@ -1,9 +1,6 @@
-from tkinter import Y, OptionMenu
-#from xml.etree.ElementPath import _Callback
 import streamlit as st
 import pandas as pd
 import numpy as np
-from streamlit_option_menu import option_menu
 from PIL import Image
 import matplotlib.pyplot as plt
 import glob
